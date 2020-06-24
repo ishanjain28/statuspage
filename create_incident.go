bc52727c108a306c79444f05b5b648d5809e6d4f
Ishan Jain <contact@ishanjain.me>
2020-06-24 21:54:08 +0530

    bugfix: Correct structure of create incident response.incident_updates.affected_components

--- create_incident.go
+++ create_incident.go
@@ -13,31 +13,31 @@ type CreateIncidentInput struct {
 }
 
 type CreateIncidentOutput struct {
-	ID                            string            `json:"id"`
-	Components                    []Components      `json:"components"`
-	CreatedAt                     time.Time         `json:"created_at"`
-	Impact                        string            `json:"impact"`
-	ImpactOverride                string            `json:"impact_override"`
-	IncidentUpdates               []IncidentUpdates `json:"incident_updates"`
-	MonitoringAt                  time.Time         `json:"monitoring_at"`
-	Name                          string            `json:"name"`
-	PageID                        string            `json:"page_id"`
-	PostmortemBody                string            `json:"postmortem_body"`
-	PostmortemBodyLastUpdatedAt   time.Time         `json:"postmortem_body_last_updated_at"`
-	PostmortemIgnored             bool              `json:"postmortem_ignored"`
-	PostmortemNotifiedSubscribers bool              `json:"postmortem_notified_subscribers"`
-	PostmortemNotifiedTwitter     bool              `json:"postmortem_notified_twitter"`
-	PostmortemPublishedAt         bool              `json:"postmortem_published_at"`
-	ResolvedAt                    time.Time         `json:"resolved_at"`
-	ScheduledAutoCompleted        bool              `json:"scheduled_auto_completed"`
-	ScheduledAutoInProgress       bool              `json:"scheduled_auto_in_progress"`
-	ScheduledFor                  time.Time         `json:"scheduled_for"`
-	ScheduledRemindPrior          bool              `json:"scheduled_remind_prior"`
-	ScheduledRemindedAt           time.Time         `json:"scheduled_reminded_at"`
-	ScheduledUntil                time.Time         `json:"scheduled_until"`
-	Shortlink                     string            `json:"shortlink"`
-	Status                        string            `json:"status"`
-	UpdatedAt                     time.Time         `json:"updated_at"`
+	ID                            string                    `json:"id"`
+	Components                    []Components              `json:"components"`
+	CreatedAt                     time.Time                 `json:"created_at"`
+	Impact                        string                    `json:"impact"`
+	ImpactOverride                string                    `json:"impact_override"`
+	IncidentUpdates               []DetailedIncidentUpdates `json:"incident_updates"`
+	MonitoringAt                  time.Time                 `json:"monitoring_at"`
+	Name                          string                    `json:"name"`
+	PageID                        string                    `json:"page_id"`
+	PostmortemBody                string                    `json:"postmortem_body"`
+	PostmortemBodyLastUpdatedAt   time.Time                 `json:"postmortem_body_last_updated_at"`
+	PostmortemIgnored             bool                      `json:"postmortem_ignored"`
+	PostmortemNotifiedSubscribers bool                      `json:"postmortem_notified_subscribers"`
+	PostmortemNotifiedTwitter     bool                      `json:"postmortem_notified_twitter"`
+	PostmortemPublishedAt         bool                      `json:"postmortem_published_at"`
+	ResolvedAt                    time.Time                 `json:"resolved_at"`
+	ScheduledAutoCompleted        bool                      `json:"scheduled_auto_completed"`
+	ScheduledAutoInProgress       bool                      `json:"scheduled_auto_in_progress"`
+	ScheduledFor                  time.Time                 `json:"scheduled_for"`
+	ScheduledRemindPrior          bool                      `json:"scheduled_remind_prior"`
+	ScheduledRemindedAt           time.Time                 `json:"scheduled_reminded_at"`
+	ScheduledUntil                time.Time                 `json:"scheduled_until"`
+	Shortlink                     string                    `json:"shortlink"`
+	Status                        string                    `json:"status"`
+	UpdatedAt                     time.Time                 `json:"updated_at"`
 }
 
 func (c Client) CreateIncident(pageID string, input *CreateIncidentInput) (*CreateIncidentOutput, error) {
